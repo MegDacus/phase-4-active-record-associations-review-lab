f8530995334f5202244bf2caa493a465b5331312
MegDacus <124758848+MegDacus@users.noreply.github.com>
2023-06-19 17:05:54 +1000

    Created many to many through relationship with models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+passenger = Passenger.create
+taxi = Taxi.create
+ride = Ride.create(passenger_id: 1, taxi_id: 1)
